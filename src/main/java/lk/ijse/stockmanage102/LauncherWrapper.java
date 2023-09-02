97b6884c20f805374ae37846392e5d55492987bf
danu <danujagreru@gmail.com>
2023-09-02 11:53:55 +0530

    dashboard UI completed

---
+++ src/main/java/lk/ijse/stockmanage102/LauncherWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.stockmanage102;
+
+/*
+    @author DanujaV
+    @created 9/2/23 - 11:50 AM   
+*/
+
+public class LauncherWrapper {
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/stockmanage102/controller/DashboardFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.stockmanage102.controller;
+
+/*
+    @author DanujaV
+    @created 9/2/23 - 11:43 AM   
+*/
+
+public class DashboardFormController {
+}
